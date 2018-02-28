bd5bc56be684bbca3accbb420c3a8ee7b70d8034
Alexander Batischev <eual.jp@gmail.com>
2018-02-28 20:42:52 +0300

    Revert "Try pagination with yesod-paginator"
    This reverts commit 656ea68a24f9cd59f90d7f1c244f893fa37ece09.

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -8,7 +8,6 @@ module Handler.Home where
 import Import
 
 import Database.Persist.Sql (fromSqlKey)
-import Yesod.Paginator      (selectPaginated)
 
 -- Define our data that will be used for creating the form.
 data FileForm = FileForm
@@ -25,7 +24,7 @@ data FileForm = FileForm
 -- inclined, or create a single monolithic file.
 getHomeR :: Handler Html
 getHomeR = do
-    (deviations, pagination) <- runDB $ selectPaginated 10 [] [Desc DeviationId]
+    deviations <- runDB $ selectList [] [Desc DeviationId]
     defaultLayout $ do
         setTitle "What We Like"
         $(widgetFile "homepage")
